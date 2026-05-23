f60597c27101491fea18c9a5d2518a6e0c7973a0
khushiweb434 <prabhakarkhushi8@gmail.com>
2026-05-23 16:42:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+function showSurprise(){
+
+document.getElementById("message")
+.classList.remove("hidden");
+
+document.getElementById("message")
+.scrollIntoView({
+
+behavior:'smooth'
+
+});
+
+}
+
+const text =
+"I Love You so much mmy love ❤️";
+
+let i = 0;
+
+function typing(){
+
+if(i < text.length){
+
+document.getElementById("typing")
+.innerHTML += text.charAt(i);
+
+i++;
+
+setTimeout(typing,100);
+
+}
+
+}
+
+typing();
