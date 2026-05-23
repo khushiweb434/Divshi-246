69681c828d4ee22bc7593cc77be2acb4f87cd02b
khushiweb434 <prabhakarkhushi8@gmail.com>
2026-05-23 17:08:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ behavior:'smooth'
 
 });
 
+/* Cute popup */
+
+setTimeout(()=>{
+
+alert("Happiest Birthday My Jaan ❤️✨");
+
+},1000);
+
 }
 
+/* Typing Text */
+
 const text =
-"I Love You so much Jaan  ❤️";
+"I Love You So Much Jaan ❤️";
 
 let i = 0;
 
@@ -33,3 +43,55 @@ setTimeout(typing,100);
 }
 
 typing();
+
+/* Floating Hearts Generator */
+
+function createHeart(){
+
+const heart =
+document.createElement("div");
+
+heart.innerHTML = "💖";
+
+heart.style.position = "fixed";
+
+heart.style.left =
+Math.random() * 100 + "vw";
+
+heart.style.top = "100vh";
+
+heart.style.fontSize =
+(Math.random() * 20 + 20) + "px";
+
+heart.style.animation =
+"floatUp 6s linear";
+
+heart.style.zIndex = "999";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},6000);
+
+}
+
+setInterval(createHeart,700);
+
+/* Cute Console Message */
+
+console.log(
+"Happy Birthday Mr. Kashyap ❤️"
+);
+
+/* Secret Click */
+
+document.addEventListener("click",()=>{
+
+console.log(
+"I Love You Forever ❤️"
+);
+
+});
